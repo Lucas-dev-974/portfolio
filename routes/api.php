7070c2417a660b37d785414c035de3a9356cda8d
lucas-lvn <lucas.lvn9439@gmail.com>
2022-10-19 20:55:36 +0400

    routes update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthentificationController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+// Authentification paths
+Route::prefix('/auth')->controller(AuthentificationController::class)->group(function(){
+    Route::post('/login', 'login')->name('login');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/logout', 'logout')->name('logout');
+    Route::post('/refresh', 'refresh')->name('refresh');
+});
+
+
+Route::get('/posts', [PostController::class, 'posts']);
+
+Route::prefix('/post')->group(function(){
+    Route::get('/', [PostController::class, 'post']);
+    Route::post('/', [PostController::class, 'create']);
+    Route::patch('/', [PostController::class, 'update']);
+    Route::delete('/', [PostController::class, 'delete']);
+
+    Route::prefix('assign')->group(function(){
+        Route::post('', [PostController::class, 'assignCateg']);
+        Route::delete('', [PostController::class, 'removeCateg']);
+    });
+});
+
+Route::prefix('/categorie')->group(function(){
+    Route::post('', [CategorieController::class, 'add']);
 });
