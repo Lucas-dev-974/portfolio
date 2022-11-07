2840005b0d9bd5a1271adf043b1b0bc3a36709b0
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-07 15:52:39 +0100

    move all route need api auth in grouped route with the midleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AuthentificationController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\MediaController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,31 +23,61 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
 // Authentification paths
 Route::prefix('/auth')->controller(AuthentificationController::class)->group(function(){
     Route::post('/login', 'login')->name('login');
     Route::post('/register', 'register')->name('register');
     Route::post('/logout', 'logout')->name('logout');
     Route::post('/refresh', 'refresh')->name('refresh');
+    
 });
+
+Route::group(['middleware' => ['auth:api'] ], function(){
+    
+    Route::get('/token/check', [AuthentificationController::class, 'tokenTest']);
+
+    Route::prefix('/post')->group(function(){
+        Route::get('/', [PostController::class, 'post']);
+        Route::post('/', [PostController::class, 'create']);
+        Route::patch('/', [PostController::class, 'update']);
+        Route::delete('/', [PostController::class, 'delete']);
+
+        Route::prefix('assign')->group(function(){
+            Route::post('', [PostController::class, 'assignCateg']);
+            Route::delete('', [PostController::class, 'removeCateg']);
+        });
+    }); 
+    
+    Route::prefix('/categorie')->group(function(){
+        Route::post('', [CategorieController::class, 'add']);
+        Route::patch('', [CategorieController::class, 'update']);
+        Route::get('/{query}', [CategorieController::class, 'search']);
+        Route::delete('/{id}', [CategorieController::class, 'delete']);
+    });    
+
+    Route::prefix('/project')->group(function(){
+        Route::get('/', [ProjectController::class, 'one']);
+        Route::post('/', [ProjectController::class, 'create']);
+        Route::patch('/', [ProjectController::class, 'update']);
+        Route::delete('/', [ProjectController::class, 'delete']);
+
+        Route::prefix('assign')->group(function(){
+            Route::post('', [ProjectController::class, 'assignCateg']);
+            Route::delete('', [ProjectController::class, 'removeCateg']);
+        });
+    });
+
+});
+
 
 
 Route::get('/posts', [PostController::class, 'posts']);
 
-Route::prefix('/post')->group(function(){
-    Route::get('/', [PostController::class, 'post']);
-    Route::post('/', [PostController::class, 'create']);
-    Route::patch('/', [PostController::class, 'update']);
-    Route::delete('/', [PostController::class, 'delete']);
+Route::get('/categories', [CategorieController::class, 'list']);
 
-    Route::prefix('assign')->group(function(){
-        Route::post('', [PostController::class, 'assignCateg']);
-        Route::delete('', [PostController::class, 'removeCateg']);
-    });
-});
+Route::get('/projects', [ProjectController::class, 'list']);
 
-Route::prefix('/categorie')->group(function(){
-    Route::post('', [CategorieController::class, 'add']);
+Route::prefix('media')->group(function(){
+    Route::get('/', [MediaController::class, 'get']);
+    Route::delete('/', [MediaController::class, 'delete']);
 });
