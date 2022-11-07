a8178a8f887b8b38f0e8e67d6b150e8bc9b6fc8a
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-07 15:50:20 +0100

    append of role field for user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -33,6 +34,9 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password',
         'remember_token',
+        'updated_at',
+        'created_at',
+        'email_verified_at'
     ];
 
     /**
