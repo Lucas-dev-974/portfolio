112d838e71088004680020b45d8a9b20ad8ec962
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-17 07:16:23 +0100

    front update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,10 +9,10 @@
     <title>protfolio-front</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
-    <script defer="defer" src="/js/chunk-vendors.13a2746d.js"></script>
-    <script defer="defer" src="/js/app.cc99a163.js"></script>
+    <script defer="defer" src="/js/chunk-vendors.3b73e70c.js"></script>
+    <script defer="defer" src="/js/app.55fab5d2.js"></script>
     <link href="/css/chunk-vendors.b81cfec1.css" rel="stylesheet">
-    <link href="/css/app.86d0bccd.css" rel="stylesheet">
+    <link href="/css/app.7eea59fd.css" rel="stylesheet">
 </head>
 
 <body class="dark"><noscript><strong>We're sorry but protfolio-front doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
