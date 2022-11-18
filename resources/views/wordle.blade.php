bdb1d641667b8535bf725d485cb25af2d45818fd
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-18 04:43:54 +0100

    front update

---
+++ resources/views/wordle.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script type="module" crossorigin src="/wordle_assets/index.f70fd9a3.js"></script>
+    <link rel="stylesheet" href="/wordle_assets/index.8a7db951.css">
+  </head>
+  <body>
+    <div id="app"></div>
+    
+  </body>
+</html>
