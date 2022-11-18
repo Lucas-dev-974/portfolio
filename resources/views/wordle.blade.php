93d42e1f152e3db560ce60bbb60e1d82644923c4
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-18 06:46:36 +0100

    code review

--- resources/views/wordle.blade.php
+++ resources/views/wordle.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite App</title>
+    <title>Wordle</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <script type="module" crossorigin src="/wordle_assets/index.f70fd9a3.js"></script>
