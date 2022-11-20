61e2f98c41c45214280041bc3965ef4e6cd597c8
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-20 08:35:26 +0100

    front update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" href="/favicon.ico">
-    <title>Lucas-lvn site web</title>
+    <title>protfolio-front</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
     <script defer="defer" src="/js/chunk-vendors.3b73e70c.js"></script>
-    <script defer="defer" src="/js/app.d4c146d8.js"></script>
+    <script defer="defer" src="/js/app.f3b9d1a1.js"></script>
     <link href="/css/chunk-vendors.b81cfec1.css" rel="stylesheet">
-    <link href="/css/app.09952a47.css" rel="stylesheet">
+    <link href="/css/app.dbe42aac.css" rel="stylesheet">
 </head>
 
 <body class="dark"><noscript><strong>We're sorry but protfolio-front doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
