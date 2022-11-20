88e45ee63d270fbca9621b5fbcd3744434fdeb8a
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-20 20:46:12 +0100

    front update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
     <script defer="defer" src="/js/chunk-vendors.7fd89ccc.js"></script>
-    <script defer="defer" src="/js/app.ce4b64b6.js"></script>
+    <script defer="defer" src="/js/app.3b5e4153.js"></script>
     <link href="/css/chunk-vendors.7d8936d6.css" rel="stylesheet">
     <link href="/css/app.a4555449.css" rel="stylesheet">
 </head>
